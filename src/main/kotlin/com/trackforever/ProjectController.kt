f50e70af04b8a070ee3d8390b26f0ae39b9d3cfc
William Cao <pastor13@uw.edu>
2018-05-22 11:27:27 -0700

    [Update] PUT and POST function headers

--- src/main/kotlin/com/trackforever/ProjectController.kt
+++ src/main/kotlin/com/trackforever/ProjectController.kt
@@ -1,10 +1,17 @@
 package com.trackforever
 
+import com.trackforever.models.TrackForeverIssue
 import com.trackforever.models.TrackForeverProject
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class ProjectController {
+
+    @Autowired
+    lateinit var projectRep: ProjectRepository
+
     @GetMapping("/projects")
     fun getProjects() = TrackForeverProject("", "", "", "", "", "", "", emptyMap())
 
@@ -17,14 +24,51 @@ class ProjectController {
 
     @GetMapping("/hashes")
     fun getHashes(): MutableMap<String, String> {
-        var temp: MutableMap<String, String> = mutableMapOf()
+        val temp: MutableMap<String, String> = mutableMapOf()
         temp.put("specific project", "fakeProj")
         temp.put("unknown project", "Invalid")
         return temp
     }
 
-    @PostMapping("/issues")
-    fun getIssue() {
+    // TODO: Handle Array of Pairs instead of Maps. :(
+
+    @PutMapping("/issues")
+    fun setIssues(@RequestBody issues: Map<String, Array<TrackForeverIssue>>): ResponseEntity<Unit> {
+        issues.keys.forEach {
+            // TODO: Check if the projectKeys actually exists. If not, return a 404 to webapp.
+            if (it == "test") { // Please change this.
+                return ResponseEntity.notFound().build()
+            }
+        }
+        return ResponseEntity.ok().build()
     }
+
+    @PutMapping("/projects")
+    fun setProjects(@RequestBody projects: Array<TrackForeverProject>): ResponseEntity<Unit> {
+        // Add to the database
+        projects.forEach {
+            projectRep.save(it)
+        }
+        return ResponseEntity.ok().build()
+    }
+
+    // Request Body looks like { projectKey, issueId } where both projectKey and issueId are Strings
+    @PostMapping("/issues")
+    fun getIssue(): TrackForeverIssue {
+
+    }
+
+    // Request Body looks like Map<String, Array<String>> where keys are projectKey and values are an array of issueIds
+    @PostMapping("/issues")
+    fun getRequestedIssues(): Map<String, Array<TrackForeverIssue>> {
+
+    }
+
+    // Request Body looks like Array<String> where each String is a projectKey
+    @PostMapping("/projects")
+    fun getRequestedProjects(): Array<TrackForeverProject> {
+
+    }
+
 
 }
