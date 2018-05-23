429184b26104a1e5b9aa52212724e6112b23e5db
William Cao <pastor13@uw.edu>
2018-05-22 17:59:13 -0700

    [Update] Documentation for each function

--- src/main/kotlin/com/trackforever/ProjectController.kt
+++ src/main/kotlin/com/trackforever/ProjectController.kt
@@ -13,6 +13,11 @@ class ProjectController {
     @Autowired
     lateinit var projectRepository: ProjectRepository
 
+    /**
+     * Gets all projects from the database.
+     * Possible Responses: HTTP 200 OK with an Array of all projects.
+     *                     HTTP 410 GONE if no projects exist in the database.
+     */
     @GetMapping("/projects")
     fun getProjects(): ResponseEntity<Array<TrackForeverProject>> {
         val projectList = ArrayList<TrackForeverProject>(projectRepository.findAll())
@@ -20,12 +25,22 @@ class ProjectController {
         return if (projectList.isEmpty()) ResponseEntity(HttpStatus.GONE) else ResponseEntity(projectArray, HttpStatus.OK)
     }
 
+    /**
+     * Gets the specified project.
+     * Possible Responses: HTTP 200 OK with the specified project.
+     *                     HTTP 410 GONE if the specified project does not exist.
+     */
     @GetMapping("/projects/{projectKey}")
     fun getProject(@PathVariable projectKey: String): ResponseEntity<TrackForeverProject> {
         val specifiedProject = projectRepository.findById(projectKey)
         return if (specifiedProject.isPresent) ResponseEntity(specifiedProject.get(), HttpStatus.OK) else ResponseEntity(HttpStatus.GONE)
     }
 
+    /**
+     * Retrieves a Map of all the projectIds and their hashes.
+     * Possible Responses: HTTP 200 OK along with a Map containing K-V pairs such that K = projectId and V = hash
+     *                     HTTP 410 GONE if no projects exist in the database
+     */
     @GetMapping("/hashes")
     fun getHashes(): ResponseEntity<Map<String, String>> {
         val projectList = ArrayList<TrackForeverProject>(projectRepository.findAll())
@@ -39,7 +54,13 @@ class ProjectController {
 
     // TODO: Handle Array of Pairs instead of Maps. :(
 
-    // Add or update existing issues
+    /**
+     * Adds or updates a project based on the the given data.
+     * Expects a mapping of K-V pairs such that K = projectKey and V = Array of Issues
+     * Possible Responses: HTTP 200 OK if all projects exist
+     *                     HTTP 410 GONE if at least one project in the key set does not exist..
+     * TODO: Add partial failure
+     */
     @PutMapping("/issues")
     fun setIssues(@RequestBody issues: Map<String, Array<TrackForeverIssue>>): ResponseEntity<Unit> {
         issues.keys.forEach {
@@ -56,6 +77,11 @@ class ProjectController {
         return ResponseEntity(HttpStatus.OK)
     }
 
+    /**
+     * Adds the given projects to the database.
+     * Possible Responses: HTTP 200 OK upon completion.
+     * TODO: Verify the project data?
+     */
     @PutMapping("/projects")
     fun setProjects(@RequestBody projects: Array<TrackForeverProject>): ResponseEntity<Unit> {
         // Add to the database
@@ -65,8 +91,12 @@ class ProjectController {
         return ResponseEntity(HttpStatus.OK)
     }
 
-    // Request Body looks like { projectKey, issueId } where both projectKey and issueId are Strings
     // TODO: Check if using Pair is okay.
+    /**
+     * Retrieves the specified issue given the projectKey and the issueId.
+     * Possible Responses: HTTP 200 OK along with the specified issue if such a projectKey and issueId exist.
+     *                     HTTP 410 GONE if either the project or issue does not exist.
+     */
     @PostMapping("/issues")
     fun getIssue(@RequestBody projectAndId: Pair<String, String>): ResponseEntity<TrackForeverIssue> {
         val specifiedProject = projectRepository.findById(projectAndId.first)
@@ -82,7 +112,13 @@ class ProjectController {
         }
     }
 
-    // Request Body looks like Map<String, Array<String>> where keys are projectKey and values are an array of issueIds
+    /**
+     * Retrieves the requested issues.
+     * Expects a Map containing K-V pairs such that K = projectKey and V = Array of issueIds.
+     * TODO: Add partial failure
+     * Possible Responses: HTTP 200 OK with a Map where K-V pairs such that K = projectKey and V = Array of issues
+     *                     HTTP 410 GONE if a project or an issue does not exist.
+     */
     @PostMapping("/issues")
     fun getRequestedIssues(@RequestBody issueIds: Map<String, Array<String>>): ResponseEntity<Map<String, Array<TrackForeverIssue>>> {
         val requestedIssues: MutableMap<String, Array<TrackForeverIssue>> = mutableMapOf()
@@ -103,7 +139,13 @@ class ProjectController {
         return ResponseEntity(requestedIssues, HttpStatus.OK)
     }
 
-    // Request Body looks like Array<String> where each String is a projectKey
+    /**
+     * Gets an Array of all requested projects.
+     * Expects an Array of projectIds that are requested.
+     * TODO: Add partial failure
+     * Possible Responses: HTTP 200 OK with an Array of all requested projects.
+     *                     HTTP 410 GONE if at least one requested project does not exist.
+     */
     @PostMapping("/projects")
     fun getRequestedProjects(@RequestBody projectIds: Array<String>): ResponseEntity<Array<TrackForeverProject>> {
         val requestedProjects: Array<TrackForeverProject> = emptyArray()
