2f8263e58ab63373e1bb288bf1a5ea4ecc9fdd23
William Cao <33009722+WilliamCao13@users.noreply.github.com>
2018-05-29 12:00:53 -0700

    Process JSON Strings (#9)
    * [Update] Modified Application and ProjectController for Initial Data injection
    
    * [Fix] Put Requests now treat input as a json string.
    Interpreting as a String.
    
    * [Update] Updated documentation to reflect changes in PUT requests
    
    * [Fix] Whoops, forgot to include Jackson Library
    
    * [Fix] POST Requests now interpret input as JSON Strings

--- src/main/kotlin/com/trackforever/controllers/ProjectController.kt
+++ src/main/kotlin/com/trackforever/controllers/ProjectController.kt
@@ -1,5 +1,9 @@
 package com.trackforever.controllers
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.trackforever.repositories.ProjectRepository
 import com.trackforever.models.TrackForeverIssue
 import com.trackforever.models.TrackForeverProject
@@ -12,11 +16,7 @@ typealias ProjectId = String
 typealias IssueId = String
 
 @RestController
-class ProjectController {
-
-    @Autowired
-    lateinit var projectRepository: ProjectRepository
-
+class ProjectController (@Autowired private var projectRepository: ProjectRepository) {
 
     data class HashResponse(val project: String, val issues: Map<IssueId, String>)
 
@@ -91,23 +91,25 @@ class ProjectController {
 
     /**
      * Adds or updates a project based on the the given data.
-     * Expects a mapping of K-V pairs such that K = projectKey and V = List of Issues
+     * Expects a mapping of K-V pairs such that K = projectKey and V = List of Issues as a JSON String
      * Possible Responses: HTTP 200 OK if all projects exist
      *                     HTTP 207 MULTI_STATUS with projectKeys mapped to an List of issues that failed to be set.
      */
     @CrossOrigin
     @PutMapping("/issues")
-    fun setIssues(@RequestBody issues: Map<ProjectId, List<TrackForeverIssue>>): ResponseEntity<Map<ProjectId, List<TrackForeverIssue>>> {
+    fun setIssues(@RequestBody issues: String): ResponseEntity<Map<ProjectId, List<TrackForeverIssue>>> {
         val failedProjects: MutableMap<ProjectId, List<TrackForeverIssue>> = mutableMapOf()
-        issues.keys.forEach {
+        val mapper = jacksonObjectMapper()
+        val content: Map<ProjectId, List<TrackForeverIssue>> = mapper.readValue(issues)
+        content.keys.forEach {
             val tFProj = projectRepository.findById(it)
             if (tFProj.isPresent) { // The projectKey does exist
-                issues[it]?.forEach { // For non-null Lists
+                content[it]?.forEach { // For non-null Lists
                     tFProj.get().issues[it.id] = it
                 }
                 projectRepository.save(tFProj.get()) // Update the entry in the database
             } else { // The projectKey doesn't exist in the database, then add it to the failed list with an empty list as its value
-                val failedIssues = issues[it]
+                val failedIssues = content[it]
                 if (failedIssues == null) {
                     failedProjects[it] = emptyList()
                 } else {
@@ -117,42 +119,47 @@ class ProjectController {
         }
         return when {
             failedProjects.isEmpty() -> ResponseEntity(HttpStatus.OK)
-            failedProjects.keys.size == issues.keys.size -> ResponseEntity(issues, HttpStatus.GONE) // None of the specified projects exist.
+            failedProjects.keys.size == content.keys.size -> ResponseEntity(content, HttpStatus.GONE) // None of the specified projects exist.
             else -> ResponseEntity(failedProjects, HttpStatus.MULTI_STATUS)
         }
     }
 
     /**
      * Adds the given projects to the database.
+     * Expects a List of projects in a JSON String.
      * Possible Responses: HTTP 200 OK upon completion.
      * TODO: Verify that the projects being passed in are valid
      */
     @CrossOrigin
     @PutMapping("/projects")
-    fun setProjects(@RequestBody projects: List<TrackForeverProject>): ResponseEntity<Unit> {
+    fun setProjects(@RequestBody projects: String): ResponseEntity<String> {
+        val mapper = jacksonObjectMapper()
+        val content: List<TrackForeverProject> = mapper.readValue(projects)
         // Add to the database
-        projects.forEach {
+        content.forEach {
             projectRepository.save(it)
         }
-        return ResponseEntity(HttpStatus.OK)
+        return ResponseEntity("Projects saved.", HttpStatus.OK)
     }
 
     /**
      * Retrieves the requested issues.
-     * Expects a Map containing K-V pairs such that K = projectKey and V = List of issueIds.
+     * Expects a Map containing K-V pairs such that K = projectKey and V = List of issueIds as a JSON String.
      * Possible Responses: HTTP 200 OK with a Map where K-V pairs such that K = projectKey and V = List of issues
      *                     HTTP 207 MULTI_STATUS with a Map of K-V pairs such that K = projectKey and V = List of issues successfully retrieved
      *                     A 207 occurs when either a single project or an issue failed to retrieve.
      */
     @CrossOrigin
     @PostMapping("/issues")
-    fun getRequestedIssues(@RequestBody issueIds: Map<ProjectId, List<IssueId>>): ResponseEntity<Map<ProjectId, List<TrackForeverIssue>>> {
+    fun getRequestedIssues(@RequestBody issueIds: String): ResponseEntity<Map<ProjectId, List<TrackForeverIssue>>> {
         val requestedIssues: MutableMap<String, List<TrackForeverIssue>> = mutableMapOf()
         var partialFailure = false
-        issueIds.keys.forEach {
+        val mapper = jacksonObjectMapper()
+        val content: Map<ProjectId, List<IssueId>> = mapper.readValue(issueIds)
+        content.keys.forEach {
             val specifiedProject = projectRepository.findById(it)
             // Ensure project exists based on the projectKey
-            if (specifiedProject.isPresent) issueIds[it]?.forEach { // Go through list of issueIds
+            if (specifiedProject.isPresent) content[it]?.forEach { // Go through list of issueIds
                 val issue = specifiedProject.get().issues[it]
                 if (issue != null) { // Check to make sure the retrieved issue isn't null
                     requestedIssues.putIfAbsent(issue.projectId, mutableListOf())
@@ -174,22 +181,24 @@ class ProjectController {
 
     /**
      * Gets a List of all requested projects.
-     * Expects an List of projectIds that are requested.
+     * Expects an List of projectIds that are requested as a JSON String.
      * Possible Responses: HTTP 200 OK with a List of all requested projects.
      *                     HTTP 207 MULTI_STATUS with a List of all requested projects that were able to be retrieved.
      */
     @CrossOrigin
     @PostMapping("/projects")
-    fun getRequestedProjects(@RequestBody projectIds: List<String>): ResponseEntity<List<TrackForeverProject>> {
+    fun getRequestedProjects(@RequestBody projectIds: String): ResponseEntity<List<TrackForeverProject>> {
         val requestedProjects: MutableList<TrackForeverProject> = mutableListOf()
-        projectIds.forEach {
+        val mapper = jacksonObjectMapper()
+        val content: List<String> = mapper.readValue(projectIds)
+        content.forEach {
             val specifiedProject = projectRepository.findById(it)
             if (specifiedProject.isPresent) {
                 requestedProjects[requestedProjects.size] = specifiedProject.get()
             }
         }
         return when {
-            requestedProjects.size != projectIds.size -> ResponseEntity(requestedProjects, HttpStatus.MULTI_STATUS)
+            requestedProjects.size != content.size -> ResponseEntity(requestedProjects, HttpStatus.MULTI_STATUS)
             else -> ResponseEntity(requestedProjects, HttpStatus.OK)
         }
     }
