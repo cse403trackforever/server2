b15a227810d546d60740615f8761fbedfc9d2196
William Cao <pastor13@uw.edu>
2018-05-29 17:32:02 -0700

    [Fix] Fix in application class too!

--- src/main/kotlin/com/trackforever/main.kt
+++ src/main/kotlin/com/trackforever/main.kt
@@ -13,9 +13,9 @@ class Application {
     @Bean
     fun init(projectRepository: ProjectRepository) = CommandLineRunner {
         val projWithNoIssues = TrackForeverProject("1234", "", "testproj1", "Will", "Test Project 1","This is a very complex project 1.", "Google Code", mutableMapOf())
-        val issueNoComments = TrackForeverIssue("issueHash123", "", "issue123", "testproj2", "fixed", listOf("defect", "bug"),
+        val issueNoComments = TrackForeverIssue("issueHash123", "", "issue123", "testproj2", "fixed", "", listOf("defect", "bug"),
                 listOf(), "Not Will", listOf("Will"), 1234, 1235, 1337)
-        val issueWithComments = TrackForeverIssue("issueHash123comments", "", "issue123comments", "testproj2", "wontfix", listOf("defect", "bug"), listOf(TrackForeverComment("Will", "Why won't you fix this?"), TrackForeverComment("Not Will", "Because I said so.")), "Will", listOf(), null, null, null)
+        val issueWithComments = TrackForeverIssue("issueHash123comments", "", "issue123comments", "testproj2", "wontfix", "", listOf("defect", "bug"), listOf(TrackForeverComment("Will", "Why won't you fix this?"), TrackForeverComment("Not Will", "Because I said so.")), "Will", listOf(), null, null, null)
         val projWithIssues = TrackForeverProject("1337", "1222", "testproj2", "Will", "Test Project 2", "This is a simple project 2.", "GitHub", mutableMapOf("issue123comments" to issueWithComments, "issue123" to issueNoComments))
 
         projectRepository.save(projWithNoIssues)
