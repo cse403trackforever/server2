dfba5dc7717829383ba887a5f7bc840f80e3fec2
William Cao <pastor13@uw.edu>
2018-06-05 12:08:01 -0700

    [Workaround] Just clears everything before now.

--- src/main/kotlin/com/trackforever/main.kt
+++ src/main/kotlin/com/trackforever/main.kt
@@ -1,10 +1,17 @@
 package com.trackforever
+import com.trackforever.repositories.ProjectRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Application
-
+class Application {
+    @Bean
+    fun init(projectRepository: ProjectRepository) = CommandLineRunner {
+        projectRepository.deleteAll()
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
