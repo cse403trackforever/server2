50f7d7bfc2b9cffcd57b32644c46b4602d099fb3
William Cao <pastor13@uw.edu>
2018-06-05 10:11:48 -0700

    [Fix] Add additional main to clear repository.

---
+++ src/main/kotlin/com/trackforever/clearDatabase.kt
@@ -0,0 +1,17 @@
+import com.trackforever.repositories.ProjectRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class Application {
+    @Bean
+    fun init(projectRepository: ProjectRepository) = CommandLineRunner {
+        projectRepository.deleteAll()
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
