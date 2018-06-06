ae526dfb0b4ed7d4a68ddca1b757a69b8ff34bb2
Christopher Addison <cjteam@cs.washington.edu>
2018-06-05 23:26:41 -0700

    [Fix] issue repo types

--- src/main/kotlin/com/trackforever/repositories/IssueRepository.kt
+++ src/main/kotlin/com/trackforever/repositories/IssueRepository.kt
@@ -4,7 +4,11 @@ import com.trackforever.models.TrackForeverIssue
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
+typealias ProjectId = String
+typealias IssueId = String
+typealias IssueKey = Pair<ProjectId, IssueId>
+
 @Repository
-interface IssueRepository : MongoRepository<TrackForeverIssue, String> {
-    fun findByprojectId(): List<TrackForeverIssue>
+interface IssueRepository : MongoRepository<TrackForeverIssue, IssueKey> {
+    fun findByProjectId(id: ProjectId): List<TrackForeverIssue>
 }
